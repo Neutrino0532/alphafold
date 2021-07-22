60eb28abceb32dae259aded11a7fbb4e0d7c93c1
Neutrino0532 <35738016+Neutrino0532@users.noreply.github.com>
2021-07-22 11:33:15 +0800

    Update install_on_local.sh

--- install_on_local.sh
+++ install_on_local.sh
@@ -5,7 +5,7 @@ conda create -n af2 python=3.8 -y
 conda activate af2
 
 conda install -y -c nvidia cudnn==8.0.4
-conda install -y -c bioconda hmmer hhsuite==3.3.0 kalign2
+conda install -y -c bioconda hmmer hhsuite==3.3.0 kalign2 -c conda-forge
 
 conda install -y -c conda-forge \
       openmm=7.5.1 \
